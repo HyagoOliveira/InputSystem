84230527a295d5dc6ce3ce9104f6f31da9c31d9a
Hyago Oliveira <hyagogow@gmail.com>
2023-10-03 09:49:28 -0300

    Add LocalizedSpriteDisplayer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - TextMesh Pro package dependency
 - InputSystem.OnDeviceInputChanged: event fired when any input from any connected device changed
 - SpriteDisplayer: replaces any occurrences of {inputName} by the corresponding TextMeshPro Sprite tag
+- LocalizedSpriteDisplayer: replaces any occurrences of {inputName} by the corresponding TextMeshPro Sprite tag in a Localization String
 - Button sprites for Xbox 360, PlayStation 4/5, Nintentdo Switch Pro Controller and Keyboard/Mouse
 
 ### Changed
---
+++ Runtime/SpriteDisplayer/LocalizedSpriteDisplayer.cs
@@ -0,0 +1,46 @@
+#if ENABLE_LOCALIZATION
+using UnityEngine;
+using UnityEngine.Localization.Components;
+
+namespace ActionCode.InputSystem
+{
+    /// <summary>
+    /// Replaces any occurrences of {inputName} by the corresponding TextMeshPro Sprite tag in a Localization String.
+    /// <para>
+    /// This components works exactly as <see cref="SpriteDisplayer"/> but using the Unity localization system.
+    /// </para>
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(LocalizeStringEvent))]
+    public sealed class LocalizedSpriteDisplayer : SpriteDisplayer
+    {
+        [Space]
+        [SerializeField, Tooltip("The local Localization component.")]
+        private LocalizeStringEvent localization;
+
+        protected override void Reset()
+        {
+            base.Reset();
+            localization = GetComponent<LocalizeStringEvent>();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            localization.OnUpdateString.AddListener(HandleLocalizationChanged);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            localization.OnUpdateString.RemoveListener(HandleLocalizationChanged);
+        }
+
+        private void HandleLocalizationChanged(string localizedText)
+        {
+            originalText = localizedText;
+            HandleDeviceInputChanged(InputSystem.LastDeviceType);
+        }
+    }
+}
+#endif
