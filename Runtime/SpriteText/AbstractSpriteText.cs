5598cf6cd5e336e80deb1b2cc9d9c01c2a2a27be
Hyago Oliveira <hyagogow@gmail.com>
2023-10-06 19:00:49 -0300

    Add AbstractSpriteText component

---
+++ Runtime/SpriteText/AbstractSpriteText.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+namespace ActionCode.InputSystem
+{
+    /// <summary>
+    /// Abstract component to display Sprites using a local <see cref="TMP_Text"/> component.
+    /// </summary>
+    [RequireComponent(typeof(TMP_Text))]
+    public abstract class AbstractSpriteText : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The local TextMeshPro component.")]
+        protected TMP_Text textMesh;
+        [SerializeField, Tooltip("The input field that will be replaced by the TMP Sprite Tag.")]
+        protected string inputField = "{input}";
+
+        /// <summary>
+        /// The original text from <see cref="textMesh"/> component.
+        /// </summary>
+        public string OriginalText { get; internal set; }
+
+        protected virtual void Reset() => textMesh = GetComponent<TMP_Text>();
+        protected virtual void Awake() => OriginalText = textMesh.text;
+        protected virtual void OnEnable() => InputSystem.OnDeviceInputChanged += HandleDeviceInputChanged;
+        protected virtual void OnDisable() => InputSystem.OnDeviceInputChanged -= HandleDeviceInputChanged;
+
+        /// <summary>
+        /// Updates the local <see cref="textMesh"/> using Sprites Tags according with the given device.
+        /// </summary>
+        /// <param name="device">The input device to update the Sprite Tags.</param>
+        public void UpdateTextWithSpriteTag(InputDeviceType device) => textMesh.text = GetTextWithSpriteTag(device);
+
+        protected abstract string GetTextWithSpriteTag(InputDeviceType device);
+
+        private void HandleDeviceInputChanged(InputDeviceType device) => UpdateTextWithSpriteTag(device);
+    }
+}
